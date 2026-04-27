41f405f7945df0c17919fe0c3f80e750cb006819
付文江 <fuwenjiang@HedyMacBook-Pro.local>
2026-04-28 00:52:19 +0800

    feat: 随机数修则修改，测试用例更新

--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -4,9 +4,11 @@ import pkg from '../../package.json'
 
 import { create } from './base/create'
 import { info } from './base/info'
+import { random } from './base/random'
 import { registerCommands } from './registerCommands'
 
 program.version(pkg.version).description(pkg.description)
 
 registerCommands(create)
 registerCommands(info)
+registerCommands(random)
